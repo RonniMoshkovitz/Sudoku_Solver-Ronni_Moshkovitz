08651bb649841da26e89b4515c1da9f09f6f2c2f
RonniMoshkovitz <117098162+RonniMoshkovitz@users.noreply.github.com>
2023-01-12 14:59:53 +0200

    Updated file Board: This class represent a sudoku board. On this board all empty cells contain the value zero, and all cells with values contain a value between 1 to the board's side length. The board keeps importent board propreties such as: side, box side, and empty cells count.

--- Sudoku Solver - Ronni Moshkovitz/BoardProcessing/Board.cs
+++ Sudoku Solver - Ronni Moshkovitz/BoardProcessing/Board.cs
@@ -6,7 +6,70 @@ using System.Threading.Tasks;
 
 namespace Sudoku_Solver___Ronni_Moshkovitz.BoardProcessing
 {
-    internal class Board
+    // This class,Board, represent a sudoku board.
+    // On this board all empty cells contain the value zero. 
+    // All cells with values contain a value between 1 to the board's side length.
+    public class Board
     {
+        // Defined values
+        private const int EMPTY_VALUE = 0;
+        public readonly int FirstValue = 1;
+
+        // Board leyput grid
+        internal readonly int[,] Grid;
+
+        //
+        internal int EmptyCount;
+
+        // Board side length
+        public int Side { get; }
+
+        // Box side length
+        public int BoxSide { get; }
+
+        // Indexer for the Board object.
+        // This indexer gives you the cell value in the given index on the grid.
+        public int this[int row, int column]
+        {
+            get { return Grid[row, column]; }
+            set { Grid[row, column] = value; }
+        }
+
+        // Constructor that initialises the Board object.
+        internal Board(int[,] layout, int boardSide)
+        {
+            Grid = layout;
+
+            Side = boardSide;
+            BoxSide = (int)Math.Sqrt(Side);
+            CountEmpty();
+        }
+
+        // This function checks if the cell in the given index is empty (contains empty value).
+        public bool IsEmptyCell(int row, int column)
+        {
+            return Grid[row, column] == EMPTY_VALUE;
+        }
+
+        // This function returnes the presentable form of the cell's value in the given index (9 -> '9', 10 -> ':').
+        public char GetPresentabeValue(int row, int column)
+        {
+            return BoardTrunslator.ToMatchingChar(Grid[row, column]);
+        }
+
+        private void CountEmpty()
+        {
+            EmptyCount = 0;
+            for (int i = 0; i < Side; i++)
+            {
+                for (int j = 0; j < Side; j++)
+                {
+                    if (IsEmptyCell(j, i))
+                        EmptyCount++;
+                }
+            }
+        }
+
     }
 }
+
