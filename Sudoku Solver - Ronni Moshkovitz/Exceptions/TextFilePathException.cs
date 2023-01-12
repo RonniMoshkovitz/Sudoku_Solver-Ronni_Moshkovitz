d8cf3d21d03ead6d520159aaba033908f694c849
RonniMoshkovitz <117098162+RonniMoshkovitz@users.noreply.github.com>
2023-01-12 14:03:44 +0200

    Updated file TextFilePathException: This abstract exception is for when the given file path is invalid for text file access. The instances include: EmptyPathException and WrongFormatException.

--- Sudoku Solver - Ronni Moshkovitz/Exceptions/TextFilePathException.cs
+++ Sudoku Solver - Ronni Moshkovitz/Exceptions/TextFilePathException.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Sudoku_Solver___Ronni_Moshkovitz.Exceptions
 {
-    internal class TextFilePathException
+    // This Exception is an exception for when the given file path is invalid for text file access.
+    public abstract class TextFilePathException : IOException
     {
+        //public TextFilePathException(string message) : base($"Invalid text file path: {message}.") { } 
+        public TextFilePathException(string message) : base(message) { }
+    }
+
+    // This Exception is an exception for when the given file path is emmpty.
+    public class EmptyPathException : TextFilePathException
+    {
+        public EmptyPathException() : base($"No path was entered") { }
+    }
+
+    // This Exception is an exception for when the given file path is invalid due to wrong file format (not *.txt).
+    public class WrongFormatException : TextFilePathException
+    {
+        public WrongFormatException(string filePath) : base($"This survice only supports text files (*.txt), you entered: \"{filePath}\"") { }
     }
 }
