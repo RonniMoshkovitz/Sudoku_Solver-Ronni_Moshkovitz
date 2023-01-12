ca5d678e9b93c22e3079a0b77b1b11907e899993
RonniMoshkovitz <117098162+RonniMoshkovitz@users.noreply.github.com>
2023-01-12 15:16:15 +0200

    Updated file BoardProcessor: This class represent an object that processes a board from an input string that represents the sudoku board, to a board object (using StringValidator, SudokuParser, and BoardValidator). Its main function generates a sudoku board from a given string, and another function returns the board's string representation for a given board.

--- Sudoku Solver - Ronni Moshkovitz/BoardProcessing/BoardProcessor.cs
+++ Sudoku Solver - Ronni Moshkovitz/BoardProcessing/BoardProcessor.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace Sudoku_Solver___Ronni_Moshkovitz.BoardProcessing
 {
+    // This class, BoardProcessor, is an object that processes a board
+    // from an input string that represents the sudoku board to a board object.
     internal class BoardProcessor
     {
+        // Validator to validate the entered string (see if can be parsed)
+        private StringValidator _strValidator = new StringValidator();
+
+        // Parser to parse the string sudoku into a sudoku grid
+        private SudokuParser _parser = new SudokuParser();
+
+        // Validator to validate the generated sudoku board (make sure the board stands by the sudoku rules)
+        private BoardValidator _validator = new BoardValidator();
+
+        // This function generates a sudoku board from a given string.
+        // may throw an error if the given string is invalid, or represents an invalid board
+        public Board ProcessBoard(string boardStr)
+        {
+            // Validats the string and returns the board's side
+            int boardSide = _strValidator.Validate(boardStr);
+
+            // Parses the string into a sudoku grid
+            int[,] boardGrid = _parser.Parse(boardStr, boardSide);
+
+            Board board = new Board(boardGrid, boardSide);
+
+            // Checks if the board is valid (stands by the sudoku rules)
+            _validator.Validate(board);
+
+            return board;
+        }
+
+        // This function returns the board's string representation.
+        public string GetBoardStr(Board board)
+        {
+            return _parser.Deparse(board.Grid);
+        }
     }
 }
