33291bf29cc449d54e5fb4e578bdf0ade16fbfb9
RonniMoshkovitz <117098162+RonniMoshkovitz@users.noreply.github.com>
2023-01-12 14:39:02 +0200

    Updated file BoardTrunslator: The class BoardTrunslator, is a Utility class. It is in charge of converting the sudoku cell's values from int value to it's matching char, and so the other way. Its 2 functions do these two convertions, in both directions.

--- Sudoku Solver - Ronni Moshkovitz/BoardProcessing/BoardTrunslator.cs
+++ Sudoku Solver - Ronni Moshkovitz/BoardProcessing/BoardTrunslator.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace Sudoku_Solver___Ronni_Moshkovitz.BoardProcessing
 {
-    internal class BoardTrunslator
+    // This class, BoardTrunslator, is a Utility class.
+    // It is in charge of converting the cell's value from int to it's matching char, and so the other way.
+    internal static class BoardTrunslator
     {
+        // Empty cell char value
+        internal const char EMPTY_CHAR = '0';
+
+        // This function converts an int value into it's matching char value.
+        internal static char ToMatchingChar(int value)
+        {
+            return (char)(EMPTY_CHAR + value);
+        }
+
+        // This function converts a char value into it's matching int value.
+        internal static int ToMatchingInt(char value)
+        {
+            return value - EMPTY_CHAR;
+        }
     }
 }
