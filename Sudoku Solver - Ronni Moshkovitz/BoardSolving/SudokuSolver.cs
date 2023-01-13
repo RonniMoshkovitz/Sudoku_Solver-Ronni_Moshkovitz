f535af67bdf7da110408628826cd4d8944e2c4e8
RonniMoshkovitz <117098162+RonniMoshkovitz@users.noreply.github.com>
2023-01-13 16:49:37 +0200

    Updated file SudokuSolver: This class represents a sudoku solver that gets a sudoku board and solves it. This class uses the other classes in this namespace to do so, and combines them.

--- Sudoku Solver - Ronni Moshkovitz/BoardSolving/SudokuSolver.cs
+++ Sudoku Solver - Ronni Moshkovitz/BoardSolving/SudokuSolver.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sudoku_Solver___Ronni_Moshkovitz.BoardProcessing;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,30 @@ using System.Threading.Tasks;
 
 namespace Sudoku_Solver___Ronni_Moshkovitz.BoardSolving
 {
-    internal class SudokuSolver
+    // This class, SudokuSolver represents a sudoku solver that gets a sudoku board and solves it.
+    public class SudokuSolver
     {
+        // A SolutionHandler to translate the exact cover solution to fill the board.
+        private SolutionHandler _handler = new SolutionHandler();
+        // A CoverMatrix to translate the board into an exact cover matrix.
+        private CoverMatrix _coverMatrixGenerator = new CoverMatrix();
+        // A DLXLayout to translate the exact cover matrix into a DLX layout (a four-way-linked list).
+        private DLXLayout _dlxLayoutGenerator = new DLXLayout();
+
+        // This function solves a given sudoku board.
+        public bool Solve(Board sudoku)
+        {
+            // Generate the sudokus matching DLX layout (a four-way-linked list).
+            ColumnNode dlxSudoku = _dlxLayoutGenerator.GenerateDLXLayout(_coverMatrixGenerator.GenerateCoverMatrix(sudoku));
+
+            // Solve using the dancing links, algoritem x metode.
+            DancingLinks dancingLinks = new DancingLinks(dlxSudoku);
+
+            // Translate the solution only if there is one.
+            if (dancingLinks.Solvable)
+                _handler.TranslateSolution(dancingLinks.Answer, sudoku);
+
+            return dancingLinks.Solvable;
+        }
     }
 }
