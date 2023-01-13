7ebd722d3451461711cce0b4fd52ac76634080aa
RonniMoshkovitz <117098162+RonniMoshkovitz@users.noreply.github.com>
2023-01-13 16:56:43 +0200

    Updated file IWriter: This interface, supports writing output. Its function writes a given output.

--- Sudoku Solver - Ronni Moshkovitz/ReadingAndWriting/IWriter.cs
+++ Sudoku Solver - Ronni Moshkovitz/ReadingAndWriting/IWriter.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace Sudoku_Solver___Ronni_Moshkovitz.ReadingAndWriting
 {
+    // This interface, IWriter, supports writing output.
     public interface IWriter
     {
+        // This function writes a given output.
+        void Write(string info);
     }
 }
