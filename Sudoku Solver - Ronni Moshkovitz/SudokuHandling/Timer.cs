d5a7702fafcf45ab15a5c149207e5b9c90cb05d3
RonniMoshkovitz <117098162+RonniMoshkovitz@users.noreply.github.com>
2023-01-13 18:28:45 +0200

    Updated file Timer: This class represents a stopwatch object. It starts and stops timing. The timing result can be accessed by the atribute Time, or TimeInMilli for millisecounds count.

--- Sudoku Solver - Ronni Moshkovitz/SudokuHandling/Timer.cs
+++ Sudoku Solver - Ronni Moshkovitz/SudokuHandling/Timer.cs
@@ -6,7 +6,48 @@ using System.Threading.Tasks;
 
 namespace Sudoku_Solver___Ronni_Moshkovitz.SudokuHandling
 {
+    // This class, Timer, represents a stopwatch object.
     internal class Timer
     {
+        // Start time of the timer.
+        private DateTime _startTime;
+
+        // The timed result in time representation format.
+        internal TimeSpan Time { get; private set; }
+
+        // The timed result in milliseconds representation format.
+        internal double TimeInMilli { get { return Time.TotalMilliseconds; } }
+
+        // Constractor for Timer object.
+        internal Timer()
+        {
+            // Initialize the time atribute.
+            Time = TimeSpan.Zero;
+        }
+
+        // This function starts the stopwatch.
+        internal void StartTimer()
+        {
+            _startTime = DateTime.Now;
+        }
+
+        // This function stops the stopwatch.
+        internal void StopTimer()
+        {
+            // Stop timing.
+            DateTime finish = DateTime.Now;
+
+            // if the stopwatch never started, time is zero.
+            if (_startTime == default)
+            {
+                Time = TimeSpan.Zero;
+            }
+            else
+            {
+                // Update time and set starting time back to defult.
+                Time = finish - _startTime;
+                _startTime = default;
+            }
+        }
     }
 }
